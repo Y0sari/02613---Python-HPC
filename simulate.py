761bbccec7c462966d6412c5c89ba704ec20599e
Yosari <18788663180@163.com>
2025-04-14 19:38:45 +0200

    task4

--- simulate.py
+++ simulate.py
@@ -2,7 +2,6 @@ from os.path import join
 import sys
 import time  
 import numpy as np
-import visualize as v
 
 def load_data(load_dir, bid):
     SIZE = 512
@@ -12,11 +11,25 @@ def load_data(load_dir, bid):
     return u, interior_mask
 
 
-def jacobi(u, interior_mask, max_iter, atol=1e-6):
-    u = np.copy(u)
+# def jacobi(u, interior_mask, max_iter, atol=1e-6):
+#     u = np.copy(u)
 
-    for i in range(max_iter):
-        # Compute average of left, right, up and down neighbors, see eq. (1)
+#     for i in range(max_iter):
+#         # Compute average of left, right, up and down neighbors, see eq. (1)
+#         u_new = 0.25 * (u[1:-1, :-2] + u[1:-1, 2:] + u[:-2, 1:-1] + u[2:, 1:-1])
+#         u_new_interior = u_new[interior_mask]
+#         delta = np.abs(u[1:-1, 1:-1][interior_mask] - u_new_interior).max()
+#         u[1:-1, 1:-1][interior_mask] = u_new_interior
+
+#         if delta < atol:
+#             break
+#     return u
+
+@profile
+def jacobi(u, interior_mask, max_iter, atol=1e-6):
+    u = np.copy(u)  # 行 16
+
+    for i in range(max_iter):  # 行 18
         u_new = 0.25 * (u[1:-1, :-2] + u[1:-1, 2:] + u[:-2, 1:-1] + u[2:, 1:-1])
         u_new_interior = u_new[interior_mask]
         delta = np.abs(u[1:-1, 1:-1][interior_mask] - u_new_interior).max()
@@ -25,6 +38,7 @@ def jacobi(u, interior_mask, max_iter, atol=1e-6):
         if delta < atol:
             break
     return u
+
 
 
 def summary_stats(u, interior_mask):
@@ -40,53 +54,40 @@ def summary_stats(u, interior_mask):
         'pct_below_15': pct_below_15,
     }
 
-
 if __name__ == '__main__':
-    # Load data
-    LOAD_DIR = 'file'
-    # with open(join(LOAD_DIR, 'building_ids.txt'), 'r') as f:
-    #     building_ids = f.read().splitlines()
+  # Load data
+  LOAD_DIR = '../modified_swiss_dwellings/'
+  with open(join(LOAD_DIR, 'building_ids.txt'), 'r') as f:
+    building_ids = f.read().splitlines()
 
-    # if len(sys.argv) < 2:
-    #     N = 1
-    # else:
-    #     N = int(sys.argv[1])
-    # building_ids = building_ids[:N]
+  if len(sys.argv) < 2:
+    N = 1
+  else:
+    N = int(sys.argv[1])
+  building_ids = building_ids[:N]
 
-    # Load floor plans
-    building_ids = ['1869', '5158']
-    N = 2
-    all_u0 = np.empty((N, 514, 514))
+  # Load floor plans
+  all_u0 = np.empty((N, 514, 514))
+  all_interior_mask = np.empty((N, 512, 512), dtype='bool')
+  for i, bid in enumerate(building_ids):
+    u0, interior_mask = load_data(LOAD_DIR, bid)
+    all_u0[i] = u0
+    all_interior_mask[i] = interior_mask
 
-    all_interior_mask = np.empty((N, 512, 512), dtype='bool')
+  # Run jacobi iterations for each floor plan
+  MAX_ITER = 20_000
+  ABS_TOL = 1e-4
 
-    # Run jacobi iterations for each floor plan
-    MAX_ITER = 20_00
-    ABS_TOL = 1e-4
+  all_u = np.empty_like(all_u0)
+  for i, (u0, interior_mask) in enumerate(zip(all_u0,
+      all_interior_mask)):
+    u = jacobi(u0, interior_mask, MAX_ITER, ABS_TOL)
+    all_u[i] = u
 
-    all_u = np.empty_like(all_u0)
-  
-    # # Print summary statistics in CSV format
-    stat_keys = ['mean_temp', 'std_temp', 'pct_above_18', 'pct_below_15', 'runtime_s']
-    print('building_id, ' + ', '.join(stat_keys))
-
-    # --- Process each building ---
-    for i, bid in enumerate(building_ids):
-        u0, interior_mask = load_data(LOAD_DIR, bid)
-        all_u0[i] = u0
-        all_interior_mask[i] = interior_mask
-
-        start_time = time.time()
-        u = jacobi(u0, interior_mask, MAX_ITER, ABS_TOL)
-        runtime_s = time.time() - start_time
-        all_u[i] = u
-
-        # v.after_plot_building(u, interior_mask)
-
-        stats = summary_stats(u, interior_mask)
-        stats['runtime_s'] = round(runtime_s, 4)
-
-        print(f"{bid},", ", ".join(str(stats[k]) for k in stat_keys))
-    
-v.after_plot_all_buildings(all_u, all_interior_mask, building_ids)
-
+  # Print summary statistics in CSV format
+  stat_keys = ['mean_temp', 'std_temp', 'pct_above_18', 'pct_below_15']
+  print('building_id, ' + ', '.join(stat_keys)) # CSV header
+  for bid, u, interior_mask in zip(building_ids, all_u,
+      all_interior_mask):
+    stats = summary_stats(u, interior_mask)
+    print(f"{bid},", ", ".join(str(stats[k]) for k in stat_keys))
